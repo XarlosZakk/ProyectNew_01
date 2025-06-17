9ae8ceaf96209c6a7d6c2d455d4cd6947385375e
XarlosZakk <carlosgamer1009@gmail.com>
2025-06-17 15:29:20 -0500

    Trabajando con React

---
+++ first-proyect/src/Greeting.js
@@ -0,0 +1,37 @@
+
+export function Greeting() {
+  return <h1>Componente de React</h1>
+}
+
+export function Usuario() {
+  return<h1>UserCard</h1>
+}
+
+/*
+ const Usuario = {
+    nombre: "carlos",
+    apellido: "serrano",
+    edad: 22,
+    telefono: "093274287472",
+    conVida: true,
+    direccion: {
+      ciudad: "Ludxemburgo",
+      calle: "av 24 plaza Victoria",
+      noCasa: "Nort-243",
+    },
+    hobbies: ["Programer", "playSoccer", "learn Lenguajes"],
+  };
+
+  //return <h1>{JSON.stringify(Usuario)}</h1>//? para mostrar el objeto
+
+  function add(x, y) {
+    return x + y;
+  }
+  return (
+    <div>
+      <h1>{Usuario.nombre}</h1>
+      <h3>{Usuario.apellido}</h3>
+      <h3>{add(5, 2)}</h3>
+    </div>
+  );
+*/
