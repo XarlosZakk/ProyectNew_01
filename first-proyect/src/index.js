d6a0065d1b7da22042c2a8b7ddd9539f9ffc9ddc
XarlosZakk <carlosgamer1009@gmail.com>
2025-06-17 15:58:13 -0500

    neuvos cambios

--- first-proyect/src/index.js
+++ first-proyect/src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import {Greeting, Usuario} from './Greeting';
-const root = ReactDom.createRoot(document.getElementById("root"));
+import ProductModule, {Navbar} from './productModule';
 
+const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Greeting />
-    <Usuario/>
+    <Greeting title='hello' />
+    <Greeting title='hola react' />
+    <Greeting title='hello js' />
+    <Greeting title='javascript good' />
+    <Greeting title='fast es la honda' />
   </>
 );
---
+++ first-proyect/src/productModule.jsx
@@ -0,0 +1,13 @@
+function ProductModule() {
+    return <div>
+        <h1>Nuevo Producto</h1>
+    </div>
+}
+
+export function Navbar() {
+    return <nav>
+        Navegacion
+    </nav>
+}
+
+export default ProductModule
